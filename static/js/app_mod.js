8edc8815dd46cc09facd8bb0acdcb127ae663d4f
Raquel Yates <raquelyates@Raquels-MacBook-Air.local>
2020-09-25 11:40:49 -0700

    module done. app_mod.js is the app for this version of index.html

--- static/js/app_mod.js
+++ static/js/app_mod.js
@@ -9,16 +9,18 @@ function buildTable(data) {
     // loop through each opject in the data
     // apend the row and cells for each value in the row
     //Lop through each field in the dataRow and add each value to a table cell (td)
-    tbody.html("");
+    tbody.html('');
     data.forEach((dataRow) => {
-        let row = tbody.append("tr");
+        let row = tbody.append('tr');
         Object.values(dataRow).forEach((val) => {
-            let call = row.append("td");
+            // add each value to a cell in the table, table data tag <td>
+            let cell = row.append('td');
+            // extract only the text of the value
             cell.text(val);
-            }
-        );
-
+        });
     });
+};
+
 
 function handleClick() {
     // grab the datetime value from the filter
@@ -28,8 +30,7 @@ function handleClick() {
     // apply ;filter to the table data to only keep rows wuth datetime values match
     if (date) {
         filteredData = filteredData.filter(row => row.datetime === date);
-    };
-
+    }
     // rebuild the table using the filtered data
     // if not data entered, table will be orgininal data
     buildTable(filteredData);
@@ -37,6 +38,7 @@ function handleClick() {
 
 // Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
+
 
 // build the table  when the page loads
 buildTable(tableData);
